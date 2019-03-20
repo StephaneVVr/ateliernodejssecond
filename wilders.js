7f63b6c89e63830dc99c0d53575bf4b650758ae3
Stéphane Vivier <contact.xsprod@gmail.com>
2019-03-20 17:13:37 +0100

    Ajout wilders.js

---
+++ wilders.js
@@ -0,0 +1,11 @@
+const chalk = require ('chalk');
+
+console.log(chalk.underline.bold.red('MY BEST TEAM OF ALL DAYS'));
+
+console.log(chalk.italic.cyan('Jonathan'));
+console.log(chalk.white('Fabien'));
+console.log(chalk.blue('Nathalie'));
+console.log(chalk.magenta('Cloé'));
+console.log(chalk.green('Marlène'));
+console.log(chalk.yellow('Sylène'));
+
